a8bebe5800aece88c653769f58e7792c1be5f419
acurling <acurling@hotmail.com>
2023-06-01 21:27:12 -0600

    Agregar archivos de proyecto.

---
+++ Arreglos2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // matrices
+
+            int[] notas = new int[4];
+            float promedio = 0f;
+            int cont = 0;
+            //// primera parte:Solicitar las notas y sacar el promedio
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.Write("Digite su nota {0}: ", i +1);
+                notas[i] = int.Parse(Console.ReadLine());
+                promedio += notas[i];
+            }
+            promedio/=notas.Length;
+            Console.WriteLine($"El promedio es de {promedio}");
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                if (notas[i]>promedio)
+                {
+                    cont++;
+                }
+
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("**************************************");
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine($"Los estudiantes con notas mayores al promedio son: {cont}");
+            Console.ReadLine();
+        }
+    }
+}
